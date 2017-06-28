b97de6d50cd4e3a1097fa7f76888a29b07e321c2
M. Ramón López Torres <manuel.lopez@zitrointeractive.com>
2017-06-28 08:28:53 +0200

    add main Class

---
+++ ObservableObject.js
@@ -0,0 +1,75 @@
+'use strict';
+/* eslint-disable no-underscore-dangle */
+
+var ObservableObject = function () {
+    this._observerTable = [];
+    this._propertiesBag = [];
+};
+
+ObservableObject.prototype.createProperty = function (property) {
+    Object.defineProperty(this, property, {
+        get: function () {
+            return this._propertiesBag[property];
+        },
+
+        set: function (value) {
+            if (this._propertiesBag[property] !== value) {
+                var oldValue = this._propertiesBag[property];
+
+                this._propertiesBag[property] = value;
+
+                for (var i = 0, n = this._observerTable.length; i < n; i++) {
+                    var boundFunction = this._observerTable[i];
+                    boundFunction(this, {property: property, oldValue: oldValue});
+                }
+            }
+        },
+
+        enumerable: true,
+        configurable: true
+    });
+};
+
+ObservableObject.prototype.addObserver = function (f) {
+    if (f && typeof f === 'function') {
+        this._observerTable.push(f);
+    }
+};
+
+ObservableObject.prototype.removeObserver = function (f) {
+    var index = this._observerTable.indexOf(f);
+
+    if (index > -1) {
+        this._observerTable.splice(index, 1);
+    }
+};
+
+ObservableObject.prototype.set = function (properyList) {
+    var changes = [];
+
+    properyList.forEach(function(keyValue) {
+        var property = keyValue.property;
+        var value = keyValue.value;
+
+        if (this._propertiesBag.hasOwnProperty(property) && this._propertiesBag[property] !== value) {
+            var oldValue = this._propertiesBag[property];
+            this._propertiesBag[property] = value;
+
+            changes.push({
+                property: property,
+                oldValue: oldValue
+            })
+        }
+    });
+
+    if (changes.length > 0) {
+        // Notify
+        for (var i = 0, n = this._observerTable.length; i < n; i++) {
+            var boundFunction = this._observerTable[i];
+            boundFunction(this, changes);
+        }
+    }
+};
+/* eslint-enable no-underscore-dangle */
+
+module.exports = ObservableObject;
